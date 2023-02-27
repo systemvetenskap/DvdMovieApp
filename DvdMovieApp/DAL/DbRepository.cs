213117f516ed8c10bf34bfbbcfb3dd8cf9b8c755
Erik Öberg <erik.oberg@miun.se>
2023-02-27 09:38:58 +0100

    Add project files.

---
+++ DvdMovieApp/DAL/DbRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvdMovieApp.DAL
+{
+    public class DbRepository
+    {
+        public void GetFilms()
+        {
+            // hämta filmer från databas
+            // eller hämta från mitt api
+        }
+    }
+}
